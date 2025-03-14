b67b0ae3abad061af6a279e96e5c4d9472f1909d
Marcin Nowak <malcinn@gmail.com>
2025-03-14 13:16:26 +0100

    Add readme, add final refinements

---
+++ README.md
@@ -0,0 +1,26 @@
+# nanoarch.py
+
+A minimal working example of using a libretro core in Python based on <a href="https://github.com/heuripedes/nanoarch/blob/master/nanoarch.c">nanoarch.c</a>.
+
+### How to run
+
+(tested on windows, linux would require slight changes - .dll -> .so etc)
+
+Install requirements from `requirements.txt` (`pip install -r requirements.txt`)
+
+Fetch `sameboy_librertro` core from https://buildbot.libretro.com/nightly/windows/x86_64/latest/sameboy_libretro.dll.zip and extract it inside the same directory as main.py
+
+Run `main.py`
+
+### Whats working
+
+- initializing libretro
+- rendering gameboy frames inside SDL2 as a texture
+
+### Whats not working
+
+- Support for cores other than Game Boy due to hardcoded resolution & pixel format (easy to modify - call `retro_get_system_av_info` and use its information)
+
+- Audio
+
+Leaving it as is to keep code minimal, feel free to implement these in your own projects
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import lib.constants
-import time
 import sdl2
 import sdl2.ext
 
@@ -8,14 +7,20 @@ from ctypes import *
 from lib.enums import retro_pixel_format
 from lib.structs import retro_game_info, retro_log_callback, retro_system_info
 
+### Configuration
+window_size = (160, 144)
+core = 'sameboy_libretro.dll'
+rom = 'demo.gb' # Source of demo.gb rom: https://buildbot.libretro.com/assets/cores/Nintendo%20-%20GameBoy
+###
+
 cmd_names = { v: k for k, v in vars(lib.constants).items() if isinstance(v, int) }
 base_dir = os.path.dirname(__file__)
 
 sdl2.ext.init()
-window = sdl2.ext.Window("RGB Buffer", size=(160, 144))
+window = sdl2.ext.Window("nanoarch.py", size=window_size)
 window.show()
 renderer = sdl2.SDL_CreateRenderer(window.window, -1, sdl2.SDL_RENDERER_ACCELERATED)
-texture = sdl2.SDL_CreateTexture(renderer, sdl2.SDL_PIXELFORMAT_ARGB8888, sdl2.SDL_TEXTUREACCESS_STREAMING, 160, 144)
+texture = sdl2.SDL_CreateTexture(renderer, sdl2.SDL_PIXELFORMAT_ARGB8888, sdl2.SDL_TEXTUREACCESS_STREAMING, window_size[0], window_size[1])
 
 # Todo: to use python logs callback variadic arguments needs to be handled somehow
 # https://cffi.readthedocs.io/en/stable/using.html#id27
@@ -26,12 +31,16 @@ def log(level, message):
 
 @CFUNCTYPE(c_bool, c_uint, c_void_p)
 def set_environment(cmd, data):
+    if cmd == lib.constants.RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE:
+        # to skipping logs since its called many times
+        return c_bool(True)
+
     cmd_name = cmd_names[cmd] if cmd in cmd_names else f'Unknown: {cmd}'
     print(f'set_environment: {cmd_name}')
 
     if cmd == lib.constants.RETRO_ENVIRONMENT_GET_LOG_INTERFACE:
         return c_bool(False)
-        # todo: If variadic arguments will be handled uncomment below lines to enable logging via log python method and comment out upper
+        # todo: If variadic arguments will be handled uncomment below lines to enable logging via log python method and comment out upper line
         # pointer = cast(data, POINTER(retro_log_callback))
         # pointer.contents.log = log
         # return c_bool(True)
@@ -67,36 +76,26 @@ def set_environment(cmd, data):
 
 @CFUNCTYPE(None, c_void_p, c_uint, c_uint, c_size_t)
 def set_video_refresh(data, width, height, pitch):
-    # ffmpeg -y -f rawvideo -pixel_format bgr0 -video_size 160x144 -i imgbuffer.raw -frames:v 1 output.png
-    # with open('imgbuffer.raw', "wb") as f:
-    #     data_ptr = cast(data, POINTER(c_ubyte))  # Cast void* to byte pointer
-    #     for y in range(height):
-    #         row_offset = y * pitch  # Move to the correct row
-    #         f.write(bytearray(data_ptr[row_offset: row_offset + (width * 4)]))  # Write only valid pixel data
-    data_ptr = cast(data, POINTER(c_ubyte))  # Cast void* to byte pointer
-    sdl2.SDL_UpdateTexture(texture, None, data_ptr, width * 4)
+    data_ptr = cast(data, POINTER(c_ubyte))
+    sdl2.SDL_UpdateTexture(texture, None, data_ptr, pitch)
 
 @CFUNCTYPE(None)
 def set_input_poll():
-    # print('set_input_poll')
     pass
 
 @CFUNCTYPE(c_int16, c_uint, c_uint, c_uint, c_uint)
 def set_input_state(port, device, index, id):
-    # print('set_input_state')
     return 0
 
 @CFUNCTYPE(None, c_short, c_short)
 def set_audio_sample(left, right):
-    # print('set_audio_sample')
     pass
 
 @CFUNCTYPE(c_size_t, c_void_p, c_size_t)
 def set_audio_sample_batch(data, frames):
-    # print('set_audio_sample_batch')
     return frames
 
-dll = cdll.LoadLibrary(os.path.join(base_dir, 'sameboy_libretro.dll'))
+dll = cdll.LoadLibrary(os.path.join(base_dir, core))
 dll.retro_set_environment(set_environment)
 dll.retro_set_video_refresh(set_video_refresh)
 dll.retro_set_input_poll(set_input_poll)
@@ -111,10 +110,6 @@ system_info_ptr = POINTER(retro_system_info)(retro_system_info())
 
 dll.retro_get_system_info(system_info_ptr)
 
-print('Need fullpath', system_info_ptr.contents.need_fullpath)
-
-rom = 'Tetris.gb'
-
 with open(rom, 'rb') as f:
     data = f.read()
 
@@ -127,22 +122,10 @@ if dll.retro_load_game(POINTER(retro_game_info)(game_info)) == 0:
     print('Failed to load game!')
     exit(1)
 
-print('retro_get_system_info called!')
-
-start_time = time.time()
-interval = 1 # displays the frame rate every 1 second
-counter = 0
-
 for i in range(1000):
     dll.retro_run()
 
-    sdl2.SDL_RenderClear(renderer)
     sdl2.SDL_RenderCopy(renderer, texture, None, None)
     sdl2.SDL_RenderPresent(renderer)
 
-    counter+=1
-    if (time.time() - start_time) > interval:
-        print("FPS: ", counter / (time.time() - start_time))
-        counter = 0
-        start_time = time.time()
 print('Done!')
