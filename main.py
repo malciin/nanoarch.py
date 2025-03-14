32452432e291782dad809ba4f5f4c8eaba4e4463
Marcin Nowak <malcinn@gmail.com>
2025-03-14 12:49:02 +0100

    Use SDL2 to render frames

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import lib.constants
 import time
+import sdl2
+import sdl2.ext
 
 from ctypes import *
 from lib.enums import retro_pixel_format
@@ -9,7 +11,13 @@ from lib.structs import retro_game_info, retro_log_callback, retro_system_info
 cmd_names = { v: k for k, v in vars(lib.constants).items() if isinstance(v, int) }
 base_dir = os.path.dirname(__file__)
 
-# Todo: handling variadic arguments
+sdl2.ext.init()
+window = sdl2.ext.Window("RGB Buffer", size=(160, 144))
+window.show()
+renderer = sdl2.SDL_CreateRenderer(window.window, -1, sdl2.SDL_RENDERER_ACCELERATED)
+texture = sdl2.SDL_CreateTexture(renderer, sdl2.SDL_PIXELFORMAT_ARGB8888, sdl2.SDL_TEXTUREACCESS_STREAMING, 160, 144)
+
+# Todo: to use python logs callback variadic arguments needs to be handled somehow
 # https://cffi.readthedocs.io/en/stable/using.html#id27
 # https://stackoverflow.com/a/13869542
 @CFUNCTYPE(None, c_uint, c_char_p)
@@ -18,14 +26,16 @@ def log(level, message):
 
 @CFUNCTYPE(c_bool, c_uint, c_void_p)
 def set_environment(cmd, data):
-    # cmd_name = cmd_names[cmd] if cmd in cmd_names else f'Unknown: {cmd}'
-    # print(f'set_environment: {cmd_name}')
+    cmd_name = cmd_names[cmd] if cmd in cmd_names else f'Unknown: {cmd}'
+    print(f'set_environment: {cmd_name}')
 
     if cmd == lib.constants.RETRO_ENVIRONMENT_GET_LOG_INTERFACE:
-        pointer = cast(data, POINTER(retro_log_callback))
-        pointer.contents.log = log
         return c_bool(False)
-    
+        # todo: If variadic arguments will be handled uncomment below lines to enable logging via log python method and comment out upper
+        # pointer = cast(data, POINTER(retro_log_callback))
+        # pointer.contents.log = log
+        # return c_bool(True)
+
     if cmd == lib.constants.RETRO_ENVIRONMENT_GET_CAN_DUPE:
         cast(data, POINTER(c_ubyte))[0] = 1
         return c_bool(True)
@@ -57,15 +67,14 @@ def set_environment(cmd, data):
 
 @CFUNCTYPE(None, c_void_p, c_uint, c_uint, c_size_t)
 def set_video_refresh(data, width, height, pitch):
-    # ffmpeg -y -f rawvideo -pixel_format rgb555le -video_size 160x144 -i imgbuffer.raw -frames:v 1 output.png
+    # ffmpeg -y -f rawvideo -pixel_format bgr0 -video_size 160x144 -i imgbuffer.raw -frames:v 1 output.png
     # with open('imgbuffer.raw', "wb") as f:
     #     data_ptr = cast(data, POINTER(c_ubyte))  # Cast void* to byte pointer
     #     for y in range(height):
     #         row_offset = y * pitch  # Move to the correct row
     #         f.write(bytearray(data_ptr[row_offset: row_offset + (width * 4)]))  # Write only valid pixel data
     data_ptr = cast(data, POINTER(c_ubyte))  # Cast void* to byte pointer
-    with open('imgbuffer.raw', 'wb') as f:
-        f.write(bytearray(data_ptr[0:width * height * 4]))
+    sdl2.SDL_UpdateTexture(texture, None, data_ptr, width * 4)
 
 @CFUNCTYPE(None)
 def set_input_poll():
@@ -126,6 +135,11 @@ counter = 0
 
 for i in range(1000):
     dll.retro_run()
+
+    sdl2.SDL_RenderClear(renderer)
+    sdl2.SDL_RenderCopy(renderer, texture, None, None)
+    sdl2.SDL_RenderPresent(renderer)
+
     counter+=1
     if (time.time() - start_time) > interval:
         print("FPS: ", counter / (time.time() - start_time))
